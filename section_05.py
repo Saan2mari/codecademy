e973eb306ea1f84be37337d7a1d3f77ee707eea4
Mari <saan2mari@gmail.com>
2015-10-31 17:41:24 +0900

    section 5

---
+++ section_05.py
@@ -0,0 +1,61 @@
+# Assign True or False as appropriate on the lines below!
+
+# Set this to True if 17 < 328 or to False if it is not.
+bool_one = 17 < 328   # We did this one for you!
+print bool_one
+
+# Set this to True if 100 == (2 * 50) or to False otherwise.
+bool_two = 100 == (2 * 50)
+print bool_two
+
+# Set this to True if 19 <= 19 or to False if it is not.
+bool_three = 19 <= 19
+print bool_three
+
+# Set this to True if -22 >= -18 or to False if it is not.
+bool_four = -22 >= -18
+print bool_four
+
+# Set this to True if 99 != (98 + 1) or to False otherwise.
+bool_five = 99 != (98 + 1)
+print bool_five
+
+
+# Assign True or False as appropriate on the lines below!
+
+# (20 - 10) > 15
+bool_one = False    # We did this one for you!
+
+# (10 + 17) == 3**16
+bool_two = (10 + 17) == 3**16
+
+# 1**2 <= -1
+bool_three = 1**2 <= -1
+
+# 40 * 4 >= -4
+bool_four = 40 * 4 >= -4
+
+# 100 != 10**2
+bool_five = 100 != 10**2
+
+
+# Assign True or False as appropriate on the lines below!
+
+# (20 - 10) > 15
+bool_one = False    # We did this one for you!
+
+# (10 + 17) == 3**16
+bool_two = (10 + 17) == 3**16
+print bool_two
+
+# 1**2 <= -1
+bool_three = 1**2 <= -1
+print bool_three
+
+# 40 * 4 >= -4
+bool_four = 40 * 4 >= -4
+print bool_four
+
+# 100 != 10**2
+bool_five = 100 != 10**2
+print bool_five
