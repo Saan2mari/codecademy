388df92c12ab9a918be26acfef7a48c31917b4e9
Mari <saan2mari@gmail.com>
2015-10-31 16:11:30 +0900

    Codecademy
    1날

---
+++ section_01.py
@@ -0,0 +1,64 @@
+# Write your code below!
+spam = 5
+my_variable = 10 
+
+a = True
+b = False
+
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+print (my_int)
+
+
+def spam():
+    eggs = 12
+    return eggs
+        
+print spam()
+
+cat = 3
+
+spam = True
+eggs = False
+
+
+# 지산아 잘해보자~ Cheer up, Mari!
+
+mysterious_variable = 42
+
+""" 잘해보자 잘해보자
+잘해보자구~
+헤헤헤
+"""
+
+
+addition = 72 + 23
+subtraction = 108 - 204
+multiplication = 108 * 0.5
+division = 108 / 9
+
+count_to = 100+200
+
+print addition
+print subtraction
+print count_to
+
+eight = 2 ** 3
+
+eggs = 10 ** 2
+
+print eight
+print eggs
+
+
+spam = 7 % 2 
+
+print spam
+
+# Bringing It All Together 입니다~
+
+monty = True
+python = 1.234
+monty_python = python ** 2
---
+++ section_03.py
@@ -0,0 +1,58 @@
+# Assign your variables below, each on its own line!
+
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+print caesar
+print praline
+print viking
+
+
+# The string below is broken. Fix it using the escape backslash!
+
+'This isn\'t flying, this is falling with style!
+
+"""
+The string "PYTHON" has six characters,
+numbered 0 to 5, as shown below:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+So if you wanted "Y", you could just type
+"PYTHON"[1] (always start counting from 0!)
+"""
+fifth_letter = "MONTY"[4]
+
+print fifth_letter
+
+
+parrot = "Norwegian Blue"
+print len(parrot) 
+
+
+parrot = "Norwegian Blue"
+print parrot.lower()
+
+parrot = "norwegian blue"
+print parrot.upper()
+
+"""Declare and assign your variable on line 4,
+then call your method on line 5!"""
+
+pi = 3.14
+print str(pi) 
+
+ministry = "The Ministry of Silly Walks"
+
+print len(ministry)
+print ministry.upper()
+
+"""Tell Python to print "Monty Python"
+to the console on line 4!"""
+
+print "Monty Python"
+
